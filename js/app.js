21ce97ab6a7abf37c68aae86965bdd9036242e84
DylanPetzer <2610dylan@gmail.com>
2022-05-20 08:54:38 +0200

    added feature and season section

--- js/app.js
+++ js/app.js
@@ -123,4 +123,16 @@ currentProductSizes.forEach((size, index) => {
         size.style.backgroundColor = "black";
         size.style.color = "white";
     });
-})
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
+});
